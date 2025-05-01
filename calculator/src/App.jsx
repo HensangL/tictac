91ee5b7d5ce6c8a334f767c9a2238c75e8667c00
HensangL <lamahensang100@gmail.com>
2025-05-01 21:21:55 +0545

    just for the green

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,27 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const [show, setshow]=useState(0)
-
-  function inc(){
-    setshow(show+1);
-  }
-  function reset(){
-    setshow(0);
-  }
+  const [firstoperand, setfirstoperand]= useState(null)
+    function display(){
+      setfirstoperand(firstoperand+1)
+    }
   return (
+    
     <div>
-      <h1 className='text-3xl text-center font-serif  '>Calculator app</h1>
-      <div className='border-2 rounded-4xl mt-20 text-center h-40 w-90 ml-140'>
-       <p className='mt-26 ml-70 text-3xl'>{show}</p>
-
-      </div>
-      <button onClick={inc}>Click me</button>
-      <button onClick={reset}>Click to reset</button>
-
-
-      
+      <p>{firstoperand}</p>
+      <button onClick={display}>Click me</button>
     </div>
   )
 }
